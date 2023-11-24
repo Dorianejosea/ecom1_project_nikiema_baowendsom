9945b8f124b7fb2f25a487710a43657318d8f0fd
Doriane NIKIEMA <Dorianejosea>
2023-11-24 10:42:48 -0500

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<h2>Bienvenu sur notre site de vente</h2>
+
+
+<!-- 
+    Formulaire permettant d'enregistrer un nouvel utilisateur
+ -->
+<form method="post" action="">
+    <input hidden name="action" value="signup">
+    <label for="user_name">Nom d'utilisateur</label>
+    <input id="user_name" name="user_name" type="text">
+    <label for="email">Email</label>
+    <input id="email" name="email" type="email">
+    <label for="pwd">Mot de passe</label>
+    <input id="pwd" name="pwd" type="password">
+    <button type="submit">S'enregistrer</button>
+</form>
